3e6b42d1dd83d5987eba7fb81a7b51a30fd976bf
Paula Chiu <chiupaula@gmail.com>
2015-09-13 16:07:33 -0700

    start building exericse 2

--- exercise-2/script.js
+++ exercise-2/script.js
@@ -1,19 +1,35 @@
-function addTodoItem(event) {
-  event.preventDefault(); // necessary to prevent the submitting of a form
-  var todolist    = document.getElementById("todolist");
-  var item        = document.getElementById("item");
+var todoInput = document.querySelector('.todo-input');
+var todoList = document.querySelector('.todo-list');
 
-  var li          = document.createElement("li");
-  var label       = document.createElement("label");
-  var checkbox    = document.createElement("input");
-  checkbox.type   = "checkbox";
-  label.appendChild(checkbox)
-  label.appendChild(document.createTextNode(item.value));
-  li.appendChild(label)
-  todolist.appendChild(li);
-  item.value = "";
+var makeTodoItem = function(e) {
+  var key = e.which || e.keyCode;
+  var li, checkbox, text, label;
+  if (key === 13) {
+    // create the list item
+    li = document.createElement('li');
+    // assign li unique id
+    li.id = Date.now();
+    // create clickable label element
+    label = document.createElement('label');
+    // store input text into variable text
+    text = document.createTextNode(this.value);
+    // create checkbox
+    checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    // put text and checkbox inside label
+    label.appendChild(checkbox);
+    label.appendChild(text);
+    // put label inside list item
+    li.appendChild(label);
+    // add list item to list
+    todoList.appendChild(li);
+    // reset text box to empty string
+    this.value = '';
+  }
 }
 
-form = document.getElementById("the-form");
-form.addEventListener("submit", addTodoItem);
-
+// add event listener on input
+todoInput.addEventListener('keyup', makeTodoItem);
+todoList.addEventListener('click', function(e) {
+  console.log(e.target)
+});
