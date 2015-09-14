72bbc27a568f52d79d9522286ed36f7eec1e3042
Paula Chiu <chiupaula@gmail.com>
2015-09-13 18:02:22 -0700

    add filter functionality

--- exercise-2/script.js
+++ exercise-2/script.js
@@ -1,12 +1,13 @@
 var todoInput = document.querySelector('.todo-input');
 var todoList = document.querySelector('.todo-list');
+var todoFilters = document.querySelector('.todo-filters');
 
 function makeTodoItem(e) {
   var key = e.which || e.keyCode;
   var li, checkbox, text, label, button, id;
   if (key === 13) {
     // store id
-    id = 'item-' + Date.now();
+    id = 'item-' + Date.now(); // id cannot start with a number
     // create the list item
     li = document.createElement('li');
     // assign li unique id
@@ -36,14 +37,44 @@ function makeTodoItem(e) {
 }
 
 function handleTodoItem(e) {
-  var label, idToRemove;
+  var li, idToRemove;
 
   if (e.target.nodeName === 'INPUT') { // handle marking and unmarking done
-    label = e.target.parentNode;
-    label.classList.toggle('done');
+    li = e.target.parentNode.parentNode;
+    li.classList.toggle('done');
   } else if (e.target.nodeName === 'BUTTON') { // handle deleting todo item
     idToRemove = e.target.dataset.remove; 
     document.querySelector('#' + idToRemove).remove();
+  }
+}
+
+function filterTodoItems(e) {
+  var filter = e.target.dataset.filter;
+  var allItems = todoList.querySelectorAll('li');
+  var filteredItems; 
+
+  if (filter === 'done') { // show done only
+    showAllTodoItems(allItems);    
+    filteredItems = todoList.querySelectorAll('li:not(.done)');
+    hideTodoItems(filteredItems);
+  } else if (filter === 'not-done') { // show not done only
+    showAllTodoItems(allItems);
+    filteredItems = todoList.querySelectorAll('li.done');
+    hideTodoItems(filteredItems);
+  } else if (filter === 'show-all') { // show all
+    showAllTodoItems(allItems);
+  }
+}
+
+function hideTodoItems(items) {
+  for (var i = 0; i < filteredItems.length; i++) {
+    filteredItems[i].classList.add('hidden');
+  }
+}
+
+function showAllTodoItems(items) {
+  for (var i = 0; i < items.length; i++) {
+    items[i].classList.remove('hidden');
   }
 }
 
@@ -51,5 +82,5 @@ function handleTodoItem(e) {
 todoInput.addEventListener('keyup', makeTodoItem);
 // add event listener to todo list
 todoList.addEventListener('click', handleTodoItem);
-
 // add event listener todo filters
+todoFilters.addEventListener('click', filterTodoItems);
